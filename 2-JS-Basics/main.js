e4e17de2f88e9dc0b488322a84a54e486730d7e0
Adrian Nowakowski <androiduserpl@gmail.com>
2018-06-05 21:37:01 +0200

    gra

--- 2-JS-Basics/main.js
+++ 2-JS-Basics/main.js
@@ -115,20 +115,28 @@ switch (job) {
 
 // zadanie
 
+/*
 var player1 = 'John';
-var height1 = 158;
+var height1 = 178;
 var age1 = 22;
 
-var player2 = "Mark";
+var player2 = 'Mark';
 var height2 = 158;
 var age2 = 22;
 
+var player3 = 'Mary';
+var height3 = 158;
+var age3 = 18;
 
 var score1 = height1 + age1 * 5;
 var score2 = height2 + age2 * 5;
+var score3 = height3 + age3 * 5;
+*/
 
-//console.log(score1 + ' '+ score2);
 
+/**** for two players ****/
+
+/*
 if (score1 == score2) {
     console.log(player1 + ' & ' + player2 + ' has the same ' + score1 + ' score. Is a mach!');
 } else if (score1 > score2) {
@@ -136,12 +144,35 @@ if (score1 == score2) {
 } else {
     console.log(player2 + ' has winn with score ' + score2);
 }
-//
-//
-//if (score1 > score2) {
-//    console.log(player1 + ' has winn with score ' + score1);
-//} else if (score1 == score2) {
-//    console.log(player1 + ' & ' + player2 + ' has the same ' + score1 + ' score. Is a mach!');
+*/
+
+
+/**** for thre players ****/
+
+//if (score1 > score2 && score1 > score3) {
+//    console.log(player1 + ' has win with ' + player2 + ' & ' + player3 + ' with score: ' + score1);
+//} else if (score2 > score1 && score2 > score3) {
+//    console.log(player2 + ' has win with ' + player1 + ' & ' + player3 + ' with score: ' + score2);
+//} else if (score3 > score1 && score3 > score2) {
+//    console.log(player3 + ' has win with ' + player2 + ' & ' + player1 + ' with score: ' + score3);
 //} else {
-//    console.log(player2 + ' has winn with score ' + score2);
+//    console.log('Some thing has go wrong :(');
 //}
+//
+//
+
+
+
+// Lesson: functions
+
+
+
+
+
+
+
+
+
+
+
+
