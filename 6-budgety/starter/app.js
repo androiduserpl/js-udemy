3a3716b4aa5ad3d22cb2ec5d36944f419fc5cd8e
Adrian Nowakowski <androiduserpl@gmail.com>
2019-02-03 14:31:42 +0100

    od nowa 2

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,38 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+    // Some code
+    
+})();
+
+
+
+// UI CONTROLLER 
+var UIControler = (function() {
+    
+    // Some code
+    
+})();
+
+
+
+
+// GLOBAL APP CONTLOLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        
+        // 1. Get filed input data
+        
+        // 2. Add item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI 
+        
+    });
+    
+})(budgetController, UIControler);
