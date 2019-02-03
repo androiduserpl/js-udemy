d52a9c13ef8615741aa338067f68276c996f7bd0
Adrian Nowakowski <androiduserpl@gmail.com>
2019-02-03 14:57:38 +0100

    od nowa 2

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,17 +1,22 @@
-
 // BUDGET CONTROLLER
-var budgetController = (function() {
-    
+var budgetController = (function () {
+
     // Some code
-    
+
 })();
 
 
 
 // UI CONTROLLER 
-var UIControler = (function() {
-    
-    // Some code
+var UIControler = (function () {
+
+    return {
+        getinput: function() {
+          var type = document.querySelector('.add__type').value; // Will be inc or exp  
+          var description = document.querySelector('.add__description').value; 
+          var value = document.querySelector('.add__value').value; 
+        }
+    };
     
 })();
 
@@ -19,20 +24,31 @@ var UIControler = (function() {
 
 
 // GLOBAL APP CONTLOLLER
-var controller = (function(budgetCtrl, UICtrl) {
-    
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var crtlAddItem = function() {
+
         // 1. Get filed input data
-        
+
         // 2. Add item to the budget controller
-        
+
         // 3. Add the item to the UI
-        
+
         // 4. Calculate the budget
-        
+
         // 5. Display the budget on the UI 
         
-    });
-    
+        console.log('kakaka');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', crtlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            crtlAddItem();
+        }
+    })
+
 })(budgetController, UIControler);
