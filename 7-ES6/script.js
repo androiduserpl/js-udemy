4706f0d38251ebf22d2da4dc523a5486f2322a71
Adrian Nowakowski <androiduserpl@gmail.com>
2019-07-31 21:09:36 +0200

    u3

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -335,6 +335,50 @@ console.log(ages.find(cur => cur >= 18));
 
 /////////////
 // Lecture: Spread operator
+/*
+
+
+function addFourAges (a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18,30,12,21);
+
+console.log(sum1);
+
+// ES5
+
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+
+console.log(sum2);
+
+
+// ES6
+
+const sum3 = addFourAges(...ages);
+console.log(sum3)
+
+const familySmith = ['Jochn', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Boob', 'Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+
+console.log(bigFamily);
+
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = '#ff00ff');
+*/
+
+
+
+
+
+
+
 
 
 
