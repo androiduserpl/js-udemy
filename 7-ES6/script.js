bc6667697d85adcd232edf370776e53a4a8f7de4
Androiduserpl <androiduserpl@gmail.com>
2019-09-05 10:37:25 +0200

    end

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -588,6 +588,7 @@ jonAthlete5.wonMedal();
 
 
 // ES6
+/*
 
 class Person6 {
     constructor(name, yearOfBirth, job) {
@@ -621,21 +622,116 @@ const johnAthlete6 = new Athlete6('Adrian', 1900, 'swimer', 3, 10);
 johnAthlete6.wonMedal();
 johnAthlete6.calculateAge();
 
+*/
 
 
+/////////////
+// Coding Challenge 8
+
+/*
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+All the report data should be printed to the console.
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+*/
+const cssBlue = 'color: blue';
+
+class Element {
+    constructor(name, buildYear) {
+        this.name = name;
+        this.buildYear = buildYear;
+    }
+}
+
+class Park extends Element {
+    constructor(name, buildYear, area, numTress) {
+        super(name, buildYear);
+        this.area = area; //km2
+        this.numTrees = numTress;
+    }
+
+    treeDensity() {
+        const density = Math.round((this.numTrees / this.area) * 100) / 100;
+        console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+    }
+}
+
+class Street extends Element {
+    constructor(name, buildYear, lenght, size = 3) {
+        super(name, buildYear);
+        this.lenght = lenght;
+        this.size = size;
+    }
+
+    clasifyStreet() {
+        const classyfication = new Map();
+        classyfication.set(1, 'tiny');
+        classyfication.set(2, 'small');
+        classyfication.set(3, 'normal');
+        classyfication.set(4, 'big');
+        classyfication.set(5, 'huge');
+
+        console.log(`${this.name}, build in ${this.buildYear}, is a ${classyfication.get(this.size)} street.`)
+    }
+}
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215),
+                 new Park('National Park', 1894, 2.9, 3541),
+                 new Park('Oak Park', 1953, 0.4, 949)];
+
+const allStreets = [ new Street('Ocean Avenue', 1999, 1.1, 4),
+                   new Street('Kaczki', 2001, 2.7, 2),
+                   new Street('Lizak', 2015, 2.5),
+                   new Street('Okoniowa', 1855, 5.1, 5)];
+
+function clalc(arr) {
+
+    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+
+    return [ sum, sum / arr.length];
+}
+
+function reportParks(p) {
+    console.log('------ Park Report ------');
+
+    //Density
+    p.forEach(el => el.treeDensity());
+
+    // Average age
+    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+    const [totalAge, avgAge] = clalc(ages)
+    console.log(`Our ${p.length} parks have an average of ${avgAge} years`)
 
 
+    // Wich park has mote than 1000 treeh
+    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} have more than 1000 trees`);
+
+}
+
+function reportStreets(s) {
+    console.log('------ Streeet Report ------');
+
+    // Total and average length of the town's streets
+    const [totalLength, avgLength] = clalc(s.map(el => el.lenght));
+    console.log(`Our ${s.length} streets have total length of ${totalLength} km, with an average of ${avgLength} km.`)
+
+    // Classify sizes
+
+    s.forEach(el => el.clasifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets)
 
 
-
-
-
-
-
-
-
-
-
-
-
+console.log('%c Adrian', cssBlue)
 
